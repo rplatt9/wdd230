5049259cf7c2bc95076665d6e667942642dee084
rplatt9 <rplatt9@byui.edu>
2022-02-02 07:59:31 -0700

    banner message

--- chamber/js/date.js
+++ chamber/js/date.js
@@ -4,6 +4,7 @@ const datefieldUK = document.querySelector("aside"); // for european/family hist
 
 // derive the current date using a date object
 const now = new Date();
+
 const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(
 	now
 );
@@ -13,3 +14,17 @@ const fulldateUK = new Intl.DateTimeFormat("en-UK", {
 // long, medium, short options ... try them
 
 datefieldUK.innerHTML = `<em>${fulldateUK}</em>`;
+
+let dayOfWeek = now.getDay();
+let bannerMessage = '';
+
+if (dayOfWeek == 1 || dayOfWeek == 2) {
+    bannerMessage = 'Come join us for the chamber meet and greet Wednesday at 7:00 p.m.';
+}
+// Step 6: Using an else statement, set the message variable to 'Woohoo!  It is the weekend!'
+else {
+    bannerMessage = '';
+}
+document.querySelector('#bannermessage').innerHTML = bannerMessage;
+console.log(dayOfWeek);
+console.log(now);
