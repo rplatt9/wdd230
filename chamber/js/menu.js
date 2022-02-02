6e1ac85e9a9378eadf7ae6ebe64677b7196220a2
rplatt9 <rplatt9@byui.edu>
2022-02-02 08:34:05 -0700

    suggestion updates

--- chamber/js/menu.js
+++ chamber/js/menu.js
@@ -1,7 +1,9 @@
 const hambutton = document.querySelector('.ham');
 const mainnav = document.querySelector('.navigation');
+const nav = document.querySelector('#navigation')
 
 hambutton.addEventListener('click', () => {mainnav.classList.toggle('responsive')}, false);
+hambutton.addEventListener('click', () => {nav.classList.toggle('navopen')}, false);
 
 // To solve the mid resizing issue with responsive class on
 window.onresize = () => {if (window.innerWidth > 760) mainnav.classList.remove('responsive')};
